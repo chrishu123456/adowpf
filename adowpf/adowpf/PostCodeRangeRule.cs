39c5eeb857584f63b226c27cdc54df96b0ba300b
Chris Huys <chrishuys@live.com>
2017-03-23 13:13:55 +0100

    validation

---
+++ adowpf/adowpf/PostCodeRangeRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Globalization;
+
+namespace adowpf
+{
+    public class PostCodeRangeRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureinfo)
+        {
+
+        }
+    }
+}
